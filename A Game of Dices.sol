4ef868692e3c1567873cfa488a3390604eadfb88
Haidy777 <haidy777@haidy777.com>
2018-02-23 18:16:02 +0100

    changes

--- A Game of Dices.sol
+++ A Game of Dices.sol
@@ -12,20 +12,25 @@ contract zeroOrOne {
             counter = 1;
         }
 
-        uint startingBlock = block.number - minMax(uint(block.blockhash(block.number - initialNonce)), 2, 255);
-        uint blocksToConsider = minMax(uint(block.blockhash(block.number - counter)), 0, startingBlock - 1);
+        uint rnd1 = uint(block.blockhash(block.number - minMax(uint(block.blockhash(block.number - initialNonce)), 2, 255)));
+        uint rnd2 = uint(block.blockhash(block.number - counter));
+        uint rnd3 = initialNonce / counter;
+
+        //pretty costly calculation (more than 200000 gas)
+        //        uint startingBlock = block.number - minMax(uint(block.blockhash(block.number - initialNonce)), 2, 255);
+        //        uint blocksToConsider = minMax(uint(block.blockhash(block.number - counter)), 0, startingBlock - 1);
         //pseudo random numbers of blocks which will be used to calculate a pseudo random number
 
-        uint randomNumber = 0;
+        //        uint randomNumber = 0;
 
-        for (uint i = 0; i <= blocksToConsider; i++) {
-            uint currentNumber = uint(block.blockhash(startingBlock + i));
+        //        for (uint i = 0; i <= blocksToConsider; i++) {
+        //            uint currentNumber = uint(block.blockhash(startingBlock + i));
+        //
+        //            randomNumber += currentNumber;
+        //could there be a out of memory problem?
+        //        }
 
-            randomNumber += currentNumber;
-            //could there be a out of memory problem?
-        }
-
-        return randomNumber;
+        return (rnd1 - rnd2) + rnd3;
     }
 
     function minMax(uint no, uint min, uint max) internal pure returns (uint) {
@@ -37,7 +42,7 @@ contract zeroOrOne {
     }
 }
 
-contract dice {
+contract DiceContract {
     zeroOrOne zoO = new zeroOrOne();
     address private owner = msg.sender;
     uint private collectedFees = 0;
@@ -48,24 +53,24 @@ contract dice {
         _;
     }
 
+    function DiceContract() public payable {}
+
     function roll() public payable {
         uint value = msg.value;
 
-        if (value * 2 < this.balance) {
-            uint willWin = zoO.rand();
+        require(value * 2 < this.balance);
 
-            if (willWin == 1) {
-                uint winAmount = value * 2;
-                uint onePercent = winAmount / 100;
+        uint willWin = zoO.rand();
 
-                // if the sender wins he gets almost double his bet
-                // 1 percent is taken for the contract developer
-                // 1 percent is submitted to the contract itself for liquidity
-                collectedFees += onePercent;
-                msg.sender.transfer(winAmount - (onePercent * 2));
-            }
-        } else {
-            revert();
+        if (willWin == 1) {
+            uint winAmount = value * 2;
+            uint onePercent = winAmount / 100;
+
+            // if the sender wins he gets almost double his bet
+            // 1 percent is taken for the contract developer
+            // 1 percent is submitted to the contract itself for liquidity
+            collectedFees += onePercent;
+            msg.sender.transfer(winAmount - (onePercent * 2));
         }
     }
 
