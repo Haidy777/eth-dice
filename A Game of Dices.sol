ce99698b96625a82f900c7e6e5b0eb8fad618cd0
Haidy777 <haidy777@haidy777.com>
2018-02-23 17:10:51 +0100

    changes

--- A Game of Dices.sol
+++ A Game of Dices.sol
@@ -2,22 +2,24 @@ pragma solidity 0.4.20;
 
 contract zeroOrOne {
     uint private initialNonce = 2;
-    uint private counter = 0;
+    uint8 private counter = 1;
 
     function randFromLastBlocks() internal returns (uint) {
         counter++;
         initialNonce = minMax((block.timestamp + block.difficulty + block.gaslimit + block.number + counter), 2, 255);
 
-        if (counter > 777) {
-            counter = 0;
+        if (counter > 255) {
+            counter = 1;
         }
 
-        uint blocksToConsider = minMax(uint(block.blockhash(block.number - initialNonce)), 2, 255);
-        //pseudo random number of blocks which will be used to calculate a pseudo random number
+        uint startingBlock = block.number - minMax(uint(block.blockhash(block.number - initialNonce)), 2, 255);
+        uint blocksToConsider = minMax(uint(block.blockhash(block.number - counter)), 0, startingBlock - 1);
+        //pseudo random numbers of blocks which will be used to calculate a pseudo random number
+
         uint randomNumber = 0;
 
         for (uint i = 0; i <= blocksToConsider; i++) {
-            uint currentNumber = uint(block.blockhash(block.number - i));
+            uint currentNumber = uint(block.blockhash(startingBlock + i));
 
             randomNumber += currentNumber;
             //could there be a out of memory problem?
@@ -30,20 +32,12 @@ contract zeroOrOne {
         return no % (min + max) - min;
     }
 
-    function main() public returns (uint) {
+    function rand() public returns (uint) {
         return minMax(randFromLastBlocks(), 0, 1);
     }
 }
 
 contract dice {
-    //https://github.com/OpenZeppelin/zeppelin-solidity/blob/master/contracts/math/SafeMath.sol#L25
-    function div(uint256 a, uint256 b) internal pure returns (uint256) {
-        // assert(b > 0); // Solidity automatically throws when dividing by 0
-        uint256 c = a / b;
-        // assert(a == b * c + a % b); // There is no case in which this doesn't hold
-        return c;
-    }
-
     zeroOrOne zoO = new zeroOrOne();
     address private owner = msg.sender;
     uint private collectedFees = 0;
@@ -58,11 +52,11 @@ contract dice {
         uint value = msg.value;
 
         if (value * 2 < this.balance) {
-            uint willWin = zoO.main();
+            uint willWin = zoO.rand();
 
             if (willWin == 1) {
                 uint winAmount = value * 2;
-                uint onePercent = div(winAmount, 100);
+                uint onePercent = winAmount / 100;
 
                 // if the sender wins he gets almost double his bet
                 // 1 percent is taken for the contract developer
@@ -75,22 +69,19 @@ contract dice {
         }
     }
 
-    function donateForGamble() public payable returns (string) {
+    function donateForGamble() public payable {
         kindDonators[msg.sender] += msg.value;
-        return "Thanks for providing funds to gamble!";
     }
 
     function getBackDonation() public {
         uint donationAmount = kindDonators[msg.sender];
 
-        if (donationAmount != 0 && collectedFees > 0) {
-            kindDonators[msg.sender] = 0;
+        require(donationAmount != 0 && collectedFees > 0);
 
-            msg.sender.transfer(donationAmount + div(collectedFees, 100));
-            // every donator gets a bonus when he gets his donation back
-        } else {
-            revert();
-        }
+        kindDonators[msg.sender] = 0;
+
+        msg.sender.transfer(donationAmount + (collectedFees / 100));
+        // every donator gets a bonus when he gets his donation back
     }
 
     function getFeeBalance() public view returns (uint){
